dc4380772eaab997410adc93ef943194ad63cc58
quesswho <sebastianmiles@live.com>
2020-11-28 23:42:59 +0100

    Missing change that didn't get pushed for whatever reason

--- Dodo/src/Core/Data/MaterialLoader.cpp
+++ Dodo/src/Core/Data/MaterialLoader.cpp
@@ -6,9 +6,6 @@ namespace Dodo
 {
 	Material* MaterialLoader::LoadMaterial(const char* path)
 	{
-		//std::string texPath = path;
-		//texPath = texPath.substr(strlen("res/"), texPath.size()); // Remove res/ because we are already inside it
-		//std::replace(texPath.begin(), texPath.end(), '/', '\\');
-		return new Material(Application::s_Application->m_AssetManager->GetShader(ShaderBuilderFlags::ShaderBuilderFlagBasicTexture), new Texture(path, 0, TextureSettings(TextureWrapMode::WRAP_CLAMP_TO_BORDER)));
+		return new Material(Application::s_Application->m_AssetManager->GetShader(ShaderBuilderFlags::ShaderBuilderFlagBasicTexture), new Texture(path, 0, TextureSettings(TextureWrapMode::WRAP_CLAMP_TO_EDGE)));
 	}
 }

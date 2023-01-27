1fd394c3123cb37fb4781a5dcbefa6ce99be65a5
quesswho <sebastianmiles@live.com>
2023-01-27 20:31:39 +0100

    PostEffects are not platform specific

---
+++ Dodo/src/Core/Graphics/PostEffect.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "PostEffect.h"
+
+namespace Dodo {
+	PostEffect::PostEffect(const FrameBufferProperties& framebufferprop, const char* path)
+		: m_Framebuffer(new FrameBuffer(framebufferprop)), m_Shader(Shader::CreateFromPath("Postfx", path))
+	{
+		Create();
+	}
+
+	void PostEffect::Create() {
+		float screenQuad[] = {
+			-1.0f,  1.0f,  0.0f, 1.0f, // top left
+			-1.0f, -1.0f,  0.0f, 0.0f, // bottom left
+				1.0f, -1.0f,  1.0f, 0.0f, // bottom right
+
+			-1.0f,  1.0f,  0.0f, 1.0f, // top left
+				1.0f, -1.0f,  1.0f, 0.0f, // bottom right
+				1.0f,  1.0f,  1.0f, 1.0f  // top right
+		};
+		m_Vertexbuffer = new VertexBuffer(screenQuad, 6 * 4 * sizeof(float), BufferProperties({ { "POSITION", 2 }, { "TEXCOORD", 2 } }));
+	}
+
+	PostEffect::~PostEffect() {
+		delete m_Shader;
+		delete m_Framebuffer;
+		delete m_Vertexbuffer;
+	}
+}

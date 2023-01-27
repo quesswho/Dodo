109845b788087efcfec3c603ff46caa18605ada7
quesswho <sebastianmiles@live.com>
2023-01-27 20:31:39 +0100

    PostEffects are not platform specific

--- Dodo/src/Core/Graphics/PostEffect.h
+++ Dodo/src/Core/Graphics/PostEffect.h
@@ -1,8 +1,43 @@
 #pragma once
 
-#ifdef DD_API_OPENGL
-#include "Platform/GraphicAPI/OpenGL/OpenGLPostEffect.h"
-namespace Dodo { typedef Platform::OpenGLPostEffect PostEffect; }
-#elif DD_API_DX11
+#include "Core/Common.h"
+#include "Core/Application/Application.h"
 
-#endif
+#include "Core/Graphics/FrameBuffer.h"
+#include "Core/Graphics/Material/Shader.h"
+#include "Core/Graphics/Buffer.h"
+
+namespace Dodo { 
+	class PostEffect {
+		private:
+			VertexBuffer* m_Vertexbuffer;
+			FrameBuffer* m_Framebuffer;
+			Shader* m_Shader;
+		public:
+			PostEffect(const FrameBufferProperties& framebufferprop, const char* path);
+			~PostEffect();
+
+			inline void Bind() const {
+				m_Framebuffer->Bind();
+			}
+
+			template<typename T>
+			void SetUniformValue(const char* location, const T val) {
+				m_Shader->Bind();
+				m_Shader->SetUniformValue(location, val);
+			}
+
+			inline void Draw(uint index = 0) const {
+				Application::s_Application->m_RenderAPI->DefaultFrameBuffer();
+				m_Shader->Bind();
+				m_Vertexbuffer->Bind();
+				m_Framebuffer->BindTexture();
+				Application::s_Application->m_RenderAPI->DrawArray(6);
+			}
+
+			void Resize(uint width, uint height) { m_Framebuffer->Resize(width, height); }
+		private:
+			void Create();
+	};
+}
+

a25fcfa7331e7a53800632d4d50976b3caf0899f
quesswho <sebbismiles@gmail.com>
2023-04-09 00:59:26 +0200

    Added Noise

--- Dodo/src/Core/Math/Random/Noise.h
+++ Dodo/src/Core/Math/Random/Noise.h
@@ -1,1 +1,11 @@
 #pragma once
+
+namespace Dodo {
+	namespace math {
+		class Noise {
+			float Simplex(float x, float y);
+
+			float SumSimplex(float x, float y, int num_iterations, float persistence, float scale);
+		};
+	}
+}

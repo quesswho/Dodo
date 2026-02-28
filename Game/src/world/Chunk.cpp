da25d73649a0eafab7edb5dab1aa703e1e787608
quesswho <sebastianmiles@live.com>
2026-02-28 01:45:52 +0100

    Subchunk stuff

--- Game/src/world/Chunk.cpp
+++ Game/src/world/Chunk.cpp
@@ -2,25 +2,36 @@
 
 Chunk::Chunk(ChunkPos chunkpos)
 	: m_ChunkPos(chunkpos)
-{
-	for (int i = 0; i < 4096; i++) {
-		m_Blocks[i] = std::make_shared<Block>(BlockType::AIR);
-	}
-}
+{}
 
-Chunk::Chunk(ChunkPos chunkpos, const std::array<Ref<Block>, 4096>& blocks)
+Chunk::Chunk(ChunkPos chunkpos, const std::unordered_map<int, std::array<Ref<Block>, 4096>>& blocks)
 	: m_ChunkPos(chunkpos), m_Blocks(blocks)
 {}
 
 BlockType Chunk::GetBlockType(int x, int y, int z) {
-	if (x >= 0 && x < 16 && y >= 0 && y < 16 && z >= 0 && z < 16) {
-		return m_Blocks[(x << 8) + (y << 4) + z]->m_Type;
+	if (x >= 0 && x < 16 && y >= 0 && z >= 0 && z < 16) {
+		auto& subchunk = m_Blocks.find(y / 16);
+		if (subchunk != m_Blocks.end()) {
+			if (subchunk->second[(x << 8) + ((y%16) << 4) + z] != nullptr) {
+				return subchunk->second[(x << 8) + ((y % 16) << 4) + z]->m_Type;
+			}
+		}
 	}
-	else {
-		return BlockType::AIR;
-	}
+	return BlockType::AIR;
 }
 
 void Chunk::SetBlockType(int x, int y, int z, BlockType type) {
-	m_Blocks[(x << 8) + (y << 4) + z]->m_Type = type;
+	if (x >= 0 && x < 16 && y >= 0 && z >= 0 && z < 16) {
+		auto& subchunk = m_Blocks.find(y / 16);
+		if (subchunk != m_Blocks.end()) {
+			subchunk->second[(x << 8) + (y << 4) + z]->m_Type =  type;
+		}
+		else {
+			std::array<Ref<Block>, 4096> subchunk;
+			subchunk[(x << 8) + ((y % 16) << 4) + z]->m_Type = type;
+			m_Blocks.emplace(y / 16, subchunk);
+		}
+
+	}
+	// Illegal block
 }

85b5b6a72c596b97f236213b7d1b3bf8bcaa7753
quesswho <sebastianmiles@live.com>
2026-03-12 04:27:49 +0100

    Added slang compiler bone

---
+++ Dodo/src/Core/Graphics/Shader/SlangCompiler.cpp
@@ -0,0 +1,12 @@
+#include "SlangCompiler.h"
+
+#include <slang.h>
+
+namespace Dodo {
+
+    ShaderSource SlangCompiler::Compile(const std::string& path)
+    {
+        
+        return {};
+    }
+}

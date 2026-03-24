beb1e5f36ddd1b8d535cc4e99870255e7e888746
quesswho <sebastianmiles20@gmail.com>
2026-03-24 13:03:39 +0100

    Data structure for slang and compiled code

--- Dodo/src/Core/Data/ShaderAsset.h
+++ Dodo/src/Core/Data/ShaderAsset.h
@@ -4,9 +4,34 @@
 #include <vector>
 
 namespace Dodo {
-    struct ShaderAsset {
+
+    /**
+     * Every different shader backend supported
+     */
+    enum class ShaderBackendTarget {
+        VulkanSPIRV,
+        OpenGLGLSL,
+        //MetalMSL,
+    };
+
+    /**
+     * Binary data for either SPIR-V or GLSL (not actually binary but it is the closest we can get to compiled code)
+     */
+    struct ShaderStageBinary {
+        ShaderStage stage;
+        std::string entryPoint;
         std::vector<uint32_t> spirv;
-        std::string glsl;
-        std::string path; // source path for hot reload
+        std::string glsl; // (OpenGL)
+    };
+
+    /**
+     * Slang shader owning compiled backend specific binaries
+     */
+    struct ShaderAsset {
+        std::string path;
+        std::string slangSource;
+
+        std::vector<ShaderStageBinary> stages;
+        // TODO: Reflection, layout maybe hash?
     };
 } // namespace Dodo

da47218a5c6d0ba31df4984c1f76342d24d63316
quesswho <sebastianmiles@live.com>
2026-03-31 14:42:23 +0200

    Fixed ThreadManager.xx

--- Dodo/src/Core/System/ThreadManager.h
+++ Dodo/src/Core/System/ThreadManager.h
@@ -2,29 +2,33 @@
 
 #include <thread>
 #include <vector>
+#include <deque>
+#include <atomic>
 
 namespace Dodo {
-
     class ThreadManager {
       private:
-        int m_Amount;
+        int m_NumThreads;
 
         std::mutex m_Mutex;
         std::condition_variable m_WorkConditional;
         std::condition_variable m_MainConditional;
-        std::vector<std::function<void()>> m_Queue;
+        std::deque<std::function<void()>> m_Queue;
 
-        bool m_Terminate;
-
+        std::atomic<bool> m_Terminate;
+        std::atomic<int> m_NumActiveJobs;
       public:
         std::vector<std::thread> m_WorkThreads;
-        ThreadManager(int amount);
+        ThreadManager(int numThreads);
         ~ThreadManager();
 
         void WaitMain();
         void Task(std::function<void()> task);
         void Terminate();
+        size_t GetQueueSize();
 
+        int GetActiveJobs() const { return m_NumActiveJobs.load(); }
+    private:
         void Loop();
     };
 } // namespace Dodo
